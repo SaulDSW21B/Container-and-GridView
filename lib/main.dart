44d64fb4c7058adce71fbb677aed59215c1c2f48
SaulDSW21B <saularevalosv@gmail.com>
2023-10-22 02:28:05 -0600

    Se agregaron tres paginas mas

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,117 @@ class PaginaUno extends StatelessWidget {
   }
 }
 
+class PaginaDos extends StatelessWidget {
+  List<String> listaDeRutasDeImagenes = [
+    'assets/zapatos.png',
+    'assets/zapatos3.png',
+    'assets/zapatos2.png',
+    'assets/zapatos7.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GridView 2'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: listaDeRutasDeImagenes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Image.asset(
+                listaDeRutasDeImagenes[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+class PaginaTres extends StatelessWidget {
+  List<String> listaDeRutasDeImagenes = [
+    'assets/zapatos.png',
+    'assets/zapatos3.png',
+    'assets/zapatos2.png',
+    'assets/zapatos7.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GridView 3'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: listaDeRutasDeImagenes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Image.asset(
+                listaDeRutasDeImagenes[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PaginaCuatro extends StatelessWidget {
+  List<String> listaDeRutasDeImagenes = [
+    'assets/zapatos.png',
+    'assets/zapatos3.png',
+    'assets/zapatos2.png',
+    'assets/zapatos7.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GridView 4'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: listaDeRutasDeImagenes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Image.asset(
+                listaDeRutasDeImagenes[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
 
 
 class MyApp extends StatelessWidget {
